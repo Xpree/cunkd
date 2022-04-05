f670306a54da10f789e6bc94dc5eb30d37da8814
Ronny-S <ronny.sundbom0@gmail.com>
2022-04-05 15:08:26 +0200

    Projectile Prefab + mats

---
+++ Assets/Scripts/KnockbackScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class KnockbackScript : MonoBehaviour
+{
+    [SerializeField] private float KnockbackStrength;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            Vector3 direction = collision.transform.position - transform.position;
+
+            rb.AddForce(direction * KnockbackStrength, ForceMode.Impulse);
+        }
+    }
+}
