eb30960dbbbc779b384f90dea56cf2398022c772
devjobe <git@devjobe.com>
2022-04-14 10:03:11 +0200

    Update nameLookAtCamera.cs

--- Assets/Scripts/nameLookAtCamera.cs
+++ Assets/Scripts/nameLookAtCamera.cs
@@ -5,6 +5,8 @@ public class nameLookAtCamera : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (Camera.main == null)
+            return;
         var cameraTransform = Camera.main.transform;
         transform.LookAt(transform.position + cameraTransform.rotation * Vector3.forward, cameraTransform.rotation * Vector3.up);
     }
