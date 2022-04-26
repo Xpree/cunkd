9ad0ef0d014f14be55e864e1897b9379a591edc0
Isak Strandberg <isakstrandberg@live.com>
2022-04-26 15:20:00 +0200

    Update GravityGun.cs

--- Assets/Scripts/Weapons/GravityGun.cs
+++ Assets/Scripts/Weapons/GravityGun.cs
@@ -264,6 +264,7 @@ public class GravityGun : NetworkBehaviour, IWeapon, IEquipable
             }
             else if (Physics.SphereCast(AnchorPoint.position, 0.5f, AnchorPoint.forward, out RaycastHit hitResult, MaxRange, TargetMask))
             {
+                pushVFX.GetComponent<VisualEffect>().Play();
                 var networkIdentity = hitResult.collider.GetComponent<NetworkIdentity>();
 
                 if (networkIdentity != null)
