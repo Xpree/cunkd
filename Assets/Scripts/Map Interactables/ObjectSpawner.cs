1a199e7f334b211393b51c5eca46cd9e6d6fbf28
5ibben <74107168+5ibben@users.noreply.github.com>
2022-04-29 15:40:44 +0200

    stuff

--- Assets/Scripts/Map Interactables/ObjectSpawner.cs
+++ Assets/Scripts/Map Interactables/ObjectSpawner.cs
@@ -121,4 +121,12 @@ public class ObjectSpawner : NetworkBehaviour, IInteractable
             Debug.Log("Nothing to interact.");
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (spawnedItem && IsPowerUpSpawner)
+        {
+            spawnedItem.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
 }
