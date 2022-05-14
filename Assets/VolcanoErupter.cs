7040760afb5a1a86f3764ff53e0345775f04ca47
5ibben <74107168+5ibben@users.noreply.github.com>
2022-05-14 20:25:43 +0200

    Made some rocks

--- Assets/VolcanoErupter.cs
+++ Assets/VolcanoErupter.cs
@@ -5,26 +5,14 @@ using UnityEngine;
 public class VolcanoErupter : MonoBehaviour
 {
     [SerializeField] float explosionForce;
-    [SerializeField] float explosionRadius;
     [SerializeField] Transform explosionPosition;
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    Rigidbody rigidbody = other.GetComponent<Rigidbody>();
-    //    if (rigidbody)
-    //    {
-    //        print("rigid enter");
-    //        rigidbody.AddForce(Vector3.up * (explosionForce / ((other.transform.position - explosionPosition.position).magnitude) + 10), ForceMode.Impulse);
-    //        rigidbody.AddExplosionForce(explosionForce, explosionPosition.position, explosionRadius);
-    //    }
-    //}
 
     private void OnTriggerStay(Collider other)
     {
         Rigidbody rigidbody = other.GetComponent<Rigidbody>();
         if (rigidbody)
         {
-            print("rigid enter");
             rigidbody.AddForce((Vector3.up * explosionForce / Mathf.Abs(other.transform.position.y - explosionPosition.position.y) + new Vector3(Random.Range(-1f,1f),0,Random.Range(-1f,1f))), ForceMode.Impulse);
         }
     }
