58e8ee7e2d407b49263464315f980639bc4c7667
5ibben <74107168+5ibben@users.noreply.github.com>
2022-05-16 14:57:49 +0200

    Made water/lava splash destroy when done playing

---
+++ Assets/Scripts/Map stuff/DestroyVFX.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DestroyVFX : MonoBehaviour
+{
+    [Header("Destroys the particle system when it stops playing\n")]
+    [SerializeField] ParticleSystem vfx;
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        if (vfx.isStopped)
+            Destroy(gameObject);
+    }
+}
--- Assets/WaterSplasher.cs
+++ Assets/WaterSplasher.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class WaterSplasher : MonoBehaviour
 {
     [SerializeField] ParticleSystem splash;
+    [SerializeField] float triggerHeight=0;
 
     private void OnTriggerEnter(Collider other)
     {
-        ParticleSystem pe = Instantiate(splash, other.transform.position, splash.transform.rotation);
+        ParticleSystem pe = Instantiate(splash, other.transform.position + new Vector3(0, triggerHeight, 0), splash.transform.rotation);
         Rigidbody rb = other.GetComponent<Rigidbody>();
         if (rb)
         {
+
             pe.transform.localScale += Vector3.one * Mathf.Log(rb.mass)/5;
         }
     }
