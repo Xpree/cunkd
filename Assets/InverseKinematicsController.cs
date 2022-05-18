6744d73464a297b0c5111fb03f849b009574ba21
Xpree <timothy.lindberg01@gmail.com>
2022-05-18 11:15:11 +0200

    added ik

---
+++ Assets/InverseKinematicsController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+public class InverseKinematicsController : MonoBehaviour
+{
+    [SerializeField] TwoBoneIKConstraint ik;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // how to change weight
+        // ik.weight = 0;
+    }
+}
