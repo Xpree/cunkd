c54af585948ac015a197c92fa4a453974ebd0fd2
xdonpetterx <donpetter@gmail.com>
2022-05-23 01:12:27 +0200

    Announcer added

--- Assets/Scripts/UI/Countdown.cs
+++ Assets/Scripts/UI/Countdown.cs
@@ -12,8 +12,11 @@ public class Countdown : MonoBehaviour
 
     public void StartCountdown(NetworkTimer networkTime)
     {
-        if(networkTime.HasTicked == false)
+        if (networkTime.HasTicked == false)
+        {
             _animator.Play("Countdown");
+            FMODUnity.RuntimeManager.PlayOneShot("event:/SoundStudents/SFX/Environment/Announcer60BPM");
+        }
     }
 
     public void StopCountdown()
