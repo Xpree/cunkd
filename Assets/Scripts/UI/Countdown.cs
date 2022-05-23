0c1bdd15c45206460b64c3ff104153723dbee9ba
devjobe <git@devjobe.com>
2022-05-23 09:59:06 +0200

    Updated Countdown Animation

--- Assets/Scripts/UI/Countdown.cs
+++ Assets/Scripts/UI/Countdown.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Countdown : MonoBehaviour
 {
     Animator _animator;
+    [SerializeField] GameObject[] _objects;
+    [SerializeField] AnimationCurve _countdownCurve;
+
     private void Awake()
     {
         _animator = GetComponent<Animator>();
@@ -14,13 +17,70 @@ public class Countdown : MonoBehaviour
     {
         if (networkTime.HasTicked == false)
         {
-            _animator.Play("Countdown");
-            FMODUnity.RuntimeManager.PlayOneShot("event:/SoundStudents/SFX/Environment/Announcer60BPM");
+            StartCoroutine(Countdown_Coroutine(networkTime));
         }
+    }
+
+    System.Collections.IEnumerator Countdown_Coroutine(NetworkTimer timer)
+    {
+        //_animator.Play("Countdown");
+        bool playedSound = false;
+
+        ResetCounters();
+
+        float t = (float)timer.Remaining;
+
+        int lastCounter = _objects.Length;
+        while (t > -1)
+        {
+            int counter = Mathf.CeilToInt(t);
+            if(lastCounter != counter)
+            {
+                if (lastCounter < _objects.Length)
+                {
+                    _objects[lastCounter].SetActive(false);
+                }
+                _objects[counter].SetActive(true);
+                lastCounter = counter;
+
+                if(counter == 3)
+                {
+                    if (!playedSound)
+                    {
+                        playedSound = true;
+                        FMODUnity.RuntimeManager.PlayOneShot("event:/SoundStudents/SFX/Environment/Announcer60BPM");
+                    }
+                }
+            }
+            
+            if(counter < _objects.Length)
+            {
+                float x = t - Mathf.Floor(t);
+                float scale = _countdownCurve.Evaluate(x);
+                _objects[counter].transform.localScale = new Vector3(scale, scale, scale);
+            }
+
+            yield return null;
+            t = (float)timer.Remaining;
+        }
+
+        ResetCounters();
+    }
+
+    private void ResetCounters()
+    {
+        for (int i = 0; i < _objects.Length; i++)
+        {
+            _objects[i].SetActive(false);
+            _objects[i].transform.localScale = Vector3.one;
+        }
+
     }
 
     public void StopCountdown()
     {
-        _animator.Play("Idle");
+        ResetCounters();
+        StopAllCoroutines();
+        //_animator.Play("Idle");
     }
 }
