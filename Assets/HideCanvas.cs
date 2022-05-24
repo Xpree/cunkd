2cb9a73ee48ea0cd12066672a42df1ad29907bb7
Alexander <alex2.aslund@gmail.com>
2022-05-24 13:41:19 +0200

    Start of animations

---
+++ Assets/HideCanvas.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCanvas : MonoBehaviour
+{
+    //private void OnMouseDown()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        this.SetActive(false);
+    //    }
+    //}
+}
---
+++ Assets/ShowCanvas.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCanvas : StateMachineBehaviour
+{
+    //public GameObject Object;
+
+    //void SetOn()
+    //{
+    //    Object.SetActive(false);
+    //}
+}
