3fd50af7aeba1e2e55a92348dba9d6c6c5146945
5ibben <74107168+5ibben@users.noreply.github.com>
2022-05-25 09:48:49 +0200

    added layermask to watersplasher to make icegadget splash properly

--- Assets/WaterSplasher.cs
+++ Assets/WaterSplasher.cs
@@ -4,26 +4,30 @@ using Mirror;
 public class WaterSplasher : MonoBehaviour
 {
     [SerializeField] GameObject splash;
+    [SerializeField] private LayerMask splashLayerMask;
     [SerializeField] float heightAdjustment=0;
 
     [Client]
     private void OnTriggerEnter(Collider other)
     {
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        if (rb)
+        if (splashLayerMask == (splashLayerMask | (1 << other.gameObject.layer)))
         {
-            //Debug.Log("splash!");
-            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/SoundStudents/SFX/Environment/Water splash sounds", rb.gameObject);
-            GameObject pe = Instantiate(splash, other.transform.position + new Vector3(0, heightAdjustment, 0), splash.transform.rotation);
-            pe.transform.localScale += Vector3.one * Mathf.Log(rb.mass) / 5;
-        }
-        else
-        {
-            rb = other.transform.parent.GetComponent<Rigidbody>();
+                Rigidbody rb = other.GetComponent<Rigidbody>();
             if (rb)
             {
+                //Debug.Log("splash!");
+                FMODUnity.RuntimeManager.PlayOneShotAttached("event:/SoundStudents/SFX/Environment/Water splash sounds", rb.gameObject);
                 GameObject pe = Instantiate(splash, other.transform.position + new Vector3(0, heightAdjustment, 0), splash.transform.rotation);
                 pe.transform.localScale += Vector3.one * Mathf.Log(rb.mass) / 5;
+            }
+            else
+            {
+                rb = other.transform.parent.GetComponent<Rigidbody>();
+                if (rb)
+                {
+                    GameObject pe = Instantiate(splash, other.transform.position + new Vector3(0, heightAdjustment, 0), splash.transform.rotation);
+                    pe.transform.localScale += Vector3.one * Mathf.Log(rb.mass) / 5;
+                }
             }
         }
     }
